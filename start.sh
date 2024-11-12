45a3dfd42cbcf499bbb7155025b0294258d0deeb
Pakzone1 <115553017+Pakzone1@users.noreply.github.com>
2024-11-13 00:06:53 +0500

    testing bot

--- start.sh
+++ start.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 
+# Update system and install basic requirements
+sudo yum update -y
+sudo yum install git -y
+sudo yum install -y nodejs
+sudo yum install -y python3
+sudo yum install -y python3-pip
+
+# Install system dependencies
+sudo yum install -y \
+    atk \
+    atk-devel \
+    at-spi2-atk \
+    cups-libs \
+    dbus-glib \
+    libXcomposite \
+    libXcursor \
+    libXdamage \
+    libXext \
+    libXi \
+    libXrandr \
+    libXScrnSaver \
+    libXtst \
+    pango \
+    pango-devel \
+    alsa-lib \
+    xorg-x11-fonts-Type1 \
+    xorg-x11-utils \
+    libxkbcommon \
+    libdrm \
+    gtk3 \
+    libgbm
+
 # Install dependencies
 npm install
 pip install -r requirements.txt
